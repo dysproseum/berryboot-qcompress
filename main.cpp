248022f8fb16c4342cf0c72de799f7ff0230e294
David Younker <dysproseum@gmail.com>
2026-04-03 02:06:57 -0400

    Accept command line arguments

--- main.cpp
+++ main.cpp
@@ -1,10 +1,52 @@
+#include <QCoreApplication>
+#include <QCommandLineParser>
+#include <QStringList>
 #include <QByteArray>
 #include <QFile>
 #include <QDebug>
 
-int main() {
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[]) {
+
+    QCoreApplication app(argc, argv);
+
+    // Set application metadata
+    QCoreApplication::setApplicationName("BerryBboot qCompress Utility");
+    QCoreApplication::setApplicationVersion("1.0.0");
+
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.setApplicationDescription("Creates distro.zsmime if distro.smime exists");
+    parser.addPositionalArgument("file", "The file to open.", "[file]");
+
+    // A boolean option with multiple names (-f, --force)
+     QCommandLineOption forceOption(QStringList() << "f" << "force",
+        QCoreApplication::translate("main", "Overwrite existing files."));
+    parser.addOption(forceOption);
+
+    // CRITICAL: Must call process() to parse arguments
+    parser.process(app);
+
+    if (parser.isSet("help")) {
+        return 0; // Exit after help is printed
+    }
+
+    // Retrieve values
+    QStringList pArgs = parser.positionalArguments();
+    if (pArgs.size() == 0) {
+        // Handle case where optional argument was not provided
+        pArgs.append("distro.smime");
+    }
+    else if (pArgs.size() > 1) {
+      qDebug() << "Too many arguments supplied.";
+      return 1;
+    }
+
     // Original data
-    QFile file("distro.smime");
+    QFile file(pArgs.at(0));
     if (!file.open(QIODevice::ReadOnly)) {
         qDebug() << "Error: file could not be opened.";
         return 1;
@@ -20,6 +62,21 @@ int main() {
 
     // Save compressed data to a file
     QFile outFile("distro.zsmime");
+
+    // Check if output exists
+    if (QFile::exists("distro.zsmime")) {
+        bool force = parser.isSet(forceOption);
+        if (!force) {
+          std::cout << "Overwrite distro.zsmime (y/n)? " << std::flush;
+          std::string confirm;
+          std::getline(std::cin, confirm); // Reads full line including spaces
+          if (confirm != "y") {
+            qDebug() << "Operation aborted.";
+            return 1;
+          }
+        }
+    }
+
     if (outFile.open(QIODevice::WriteOnly)) {
         outFile.write(compressedData);
         outFile.close();
